f5f32071c7b43cc722851778e3611619dcdeea56
yenock <yenockmailbox@gmail.com>
2022-11-16 22:13:34 +0530

    first commit

---
+++ src/com/base_class/Adactin_base_class.java
@@ -0,0 +1,30 @@
+package com.base_class;
+
+public class Adactin_base_class {
+
+
+//	public static webDriver driver;
+
+	public static void getBrowser() {
+
+	//	if (type.equalsIgnoreCase("chrome")) {
+
+			System.setProperty
+			("webdriver.chrome.driver", "C:\\Users\\yenoc\\Selenium_Workspace\\Selenium\\Driver\\chromedriver.exe");
+			
+			driver = new ChromeDriver();
+		}
+
+
+//		else if (type.equalsIgnoreCase("firefox")) {
+//
+//			System.setProperty
+//			("user.dir", "C:\\Users\\yenoc\\Selenium_Workspace\\Selenium\\Driver\\chromedriver.exe");
+//			driver = new FirefoxDriver();
+
+			}
+
+
+
+
+
